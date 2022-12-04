6d50acffbaa717d5bf36816ddfcc5cef2246bf42
PeterLun <peterlun1002@yahoo.com>
2022-12-04 18:50:31 +0800

    add images

---
+++ src/Components/ProductList.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function ProductList() {
+  return (
+    <div>
+      <h1>Purchase your moblie Pone</h1>
+      <h1>Product Information</h1>
+    </div>
+  )
+}
---
+++ src/Components/Shared/Products.js
@@ -0,0 +1,122 @@
+
+export const PRODUCTS =
+[
+    {
+        "id": 0,
+        "brand": "Xiaomi 小米",
+        "product": "12T Pro 5G",
+        "color": "Black",
+        "GB": "256GB",
+        "price": 4798,
+        "images" : "./Assets/Images/xiaomi_black.png"
+    },
+    {
+        "id": 1,
+        "brand": "Xiaomi 小米",
+        "product": "12T Pro 5G",
+        "color": "Blue",
+        "GB": "256GB",
+        "price": 4798,
+        "images" : "./Assets/Images/xiaomi_blue.png"
+    },
+    {
+        "id": 2,
+        "brand": "Xiaomi 小米",
+        "product": "12T Pro 5G",
+        "color": "White",
+        "GB": "256GB",
+        "price": 4798,
+        "images" : "./Assets/Images/xiaomi_white.png"
+    },
+	{
+        "id": 3,
+        "brand": "Apple",
+        "product": "iPhone 14",
+        "color": "Black",
+        "GB": "256GB",
+        "price": 7699,
+        "images" : "./Assets/Images/iphone14_black.png"
+    },
+	{
+        "id": 4,
+        "brand": "Apple",
+        "product": "iPhone 14",
+        "color": "Blue",
+        "GB": "256GB",
+        "price": 7699,
+        "images" : "./Assets/Images/iphone14_blue.png"
+    },
+	{
+        "id": 5,
+        "brand": "Apple",
+        "product": "iPhone 14",
+        "color": "Purple",
+        "GB": "256GB",
+        "price": 7699,
+        "images" : "./Assets/Images/iphone14_purple.png"
+    },
+	{
+        "id": 6,
+        "brand": "Apple",
+        "product": "iPhone 14",
+        "color": "Red",
+        "GB": "256GB",
+        "price": 7699,
+        "images" : "./Assets/Images/iphone14_red.png"
+    },
+	{
+        "id": 7,
+        "brand": "Apple",
+        "product": "iPhone 14 Pro Max",
+        "color": "Black",
+        "GB": "256GB",
+        "price": 11780,
+        "images" : "./Assets/Images/iphone14promax_black.png"
+    },
+	{
+        "id": 8,
+        "brand": "Apple",
+        "product": "iPhone 14 Pro Max",
+        "color": "Gold",
+        "GB": "256GB",
+        "price": 11780,
+        "images" : "./Assets/Images/iphone14promax_gold.png"
+    },
+	{
+        "id": 9,
+        "brand": "Apple",
+        "product": "iPhone 14 Pro Max",
+        "color": "Purple",
+        "GB": "256GB",
+        "price": 11780,
+        "images" : "./Assets/Images/iphone14promax_purple.png"
+    },
+	{
+        "id": 10,
+        "brand": "Apple",
+        "product": "iPhone 14 Pro Max",
+        "color": "White",
+        "GB": "256GB",
+        "price": 11780,
+        "images" : "./Assets/Images/iphone14promax_white.png"
+    },
+	{
+        "id": 11,
+        "brand": "SamSung",
+        "product": "Galaxy A33",
+        "color": "White",
+        "GB": "128GB",
+        "price": 2270,
+        "images" : "./Assets/Images/samsung_Galaxy_A33.png"
+    },
+	{
+        "id": 12,
+        "brand": "SamSung",
+        "product": "Galaxy A42 5G",
+        "color": "Black",
+        "GB": "128GB",
+        "price": 2390,
+        "images" : "./Assets/Images/samsung_black.png"
+    },	
+
+];
